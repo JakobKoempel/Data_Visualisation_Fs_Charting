1e0babcd440d27d6ea4dcbb5bb26d2ea022f6fcf
Jakob Kömpel <jakob.koempel@gmail.com>
2018-04-23 22:09:16 +0800

    tuple date presented in a line and point chart

---
+++ data_Visualisation_with_Fs_Charting/line_chart_tuple_sequence/Program.fs
@@ -0,0 +1,15 @@
+﻿open FSharp.Charting
+
+let tupleSeqToLineChart (s : (float * float) seq) : ChartTypes.GenericChart =
+    Chart.Line ([for i in 0 .. (Seq.length s) - 1 -> fst (Seq.item i s), snd (Seq.item i s) ])
+
+let tupleSeqToPointChart (s : (float * float) seq) : ChartTypes.GenericChart =
+    Chart.Point ([for i in 0 .. (Seq.length s) - 1 -> fst (Seq.item i s), snd (Seq.item i s) ])
+
+let data : (float * float) list = [(5.0 , 8.0) ; (1.0 , 6.5) ; (10.0 , 17.3) ; (6.0 , 13.2)]
+
+let chart = Chart.Combine (
+               [ tupleSeqToLineChart <| Seq.sortBy (fun (x, _) -> x) data
+                 tupleSeqToPointChart data]
+            )
+System.Windows.Forms.Application.Run(chart.ShowChart())
