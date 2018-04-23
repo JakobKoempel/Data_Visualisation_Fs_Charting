759c01143daabf689eedcab71db5fd6cd5e6e442
Jakob Kömpel <jakob.koempel@gmail.com>
2018-04-23 19:34:29 +0800

    basic line graph

---
+++ data_Visualisation_with_Fs_Charting/line_chart_basic/Program.fs
@@ -0,0 +1,14 @@
+﻿open FSharp.Charting
+
+let rec factorial (n : int) : int =
+    if (n = 0) then 1
+    else n * factorial (n - 1)
+
+let combine = Chart.Combine(
+                  [
+                    Chart.Line([for x in 1..10 -> x , x * x], Name = "quadradic function")
+                    Chart.Line ([for x in 1..5 -> x, factorial x], Name = "factorial function")
+                    Chart.Line ([for x in 1..10 -> x , x], Name = "linear function")
+                  ])
+
+System.Windows.Forms.Application.Run(combine.ShowChart())
