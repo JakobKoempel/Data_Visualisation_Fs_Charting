15b7da2fe22247bfcfa02c9009cc1f1396658483
Jakob Kömpel <jakob.koempel@gmail.com>
2018-04-23 18:33:34 +0800

    combined line graphs

--- data_Visualisation_with_Fs_Charting/lineGraph/Program.fs
+++ data_Visualisation_with_Fs_Charting/lineGraph/Program.fs
@@ -1,5 +1,15 @@
 ﻿open FSharp.Charting
 
-let lineChart = Chart.Line [for x in 1..10 -> x , x * x]
+let rec factorial (n : int) : int =
+    if (n = 0) then 1
+    else n * factorial (n - 1)
 
-System.Windows.Forms.Application.Run(lineChart.ShowChart())
+let lineChart1 = 
+let lineChart2 = Chart.Line ([for x in 1..5 -> x, factorial x], Name = "factorial function")
+let lineChart3 = Chart.Line ([for x in 1..10 -> x , x], Name = "linear function")
+
+let combine = Chart.Combine([
+                    Chart.Line ([for x in 1..10 -> x , x * x], Name = "quadradic function") ; lineChart2
+                    lineChart3])
+
+System.Windows.Forms.Application.Run(combine.ShowChart())
