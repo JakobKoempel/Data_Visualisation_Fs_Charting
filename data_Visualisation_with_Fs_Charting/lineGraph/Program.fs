307bc8daf1beffd62998567d0c81286c099d87a2
Jakob Kömpel <jakob.koempel@gmail.com>
2018-04-23 18:43:51 +0800

    line graph with fs charting

--- data_Visualisation_with_Fs_Charting/lineGraph/Program.fs
+++ data_Visualisation_with_Fs_Charting/lineGraph/Program.fs
@@ -4,12 +4,10 @@ let rec factorial (n : int) : int =
     if (n = 0) then 1
     else n * factorial (n - 1)
 
-let lineChart1 = 
-let lineChart2 = Chart.Line ([for x in 1..5 -> x, factorial x], Name = "factorial function")
-let lineChart3 = Chart.Line ([for x in 1..10 -> x , x], Name = "linear function")
-
 let combine = Chart.Combine([
-                    Chart.Line ([for x in 1..10 -> x , x * x], Name = "quadradic function") ; lineChart2
-                    lineChart3])
+                    Chart.Line([for x in 1..10 -> x , x * x], Name = "quadradic function")
+                    Chart.Line ([for x in 1..5 -> x, factorial x], Name = "factorial function")
+                    Chart.Line ([for x in 1..10 -> x , x], Name = "linear function")
+                    ])
 
 System.Windows.Forms.Application.Run(combine.ShowChart())
