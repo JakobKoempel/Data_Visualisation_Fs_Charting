39efb59045b976826406dde640227a9fca6c955b
Jakob Kömpel <jakob.koempel@gmail.com>
2018-04-23 22:14:45 +0800

    simplified code

--- data_Visualisation_with_Fs_Charting/line_chart_tuple_sequence/Program.fs
+++ data_Visualisation_with_Fs_Charting/line_chart_tuple_sequence/Program.fs
@@ -4,7 +4,7 @@ let tupleSeqToLineChart (s : (float * float) seq) : ChartTypes.GenericChart =
     Chart.Line ([for i in 0 .. (Seq.length s) - 1 -> fst (Seq.item i s), snd (Seq.item i s) ])
 
 let tupleSeqToPointChart (s : (float * float) seq) : ChartTypes.GenericChart =
-    Chart.Point ([for i in 0 .. (Seq.length s) - 1 -> fst (Seq.item i s), snd (Seq.item i s) ])
+    Chart.Point ([for i in 0 .. (Seq.length s) - 1 -> fst <| Seq.item i s , snd <| Seq.item i s])
 
 let data : (float * float) list = [(5.0 , 8.0) ; (1.0 , 6.5) ; (10.0 , 17.3) ; (6.0 , 11.2)]
 
