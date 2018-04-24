d392a859142f5a6f3050fe586fbd3d88471ed06f
Jakob Kömpel <jakob.koempel@gmail.com>
2018-04-24 23:34:58 +0800

    simplified code

--- data_Visualisation_with_Fs_Charting/simple_linear_regression/Program.fs
+++ data_Visualisation_with_Fs_Charting/simple_linear_regression/Program.fs
@@ -2,7 +2,7 @@
 
 let dataSet = [(1. , 2.) ; (1. , 4.) ; (2. , 5.) ; (3. , 5.) ; (3.3 , 5.5) ; (4. , 4.) ; (4.3, 5.3) ; (5. , 5.)]
 
-let slr (s : (float * float) seq) : float -> float=
+let simpleLinearRegression (s : (float * float) seq) : float -> float=
     let xSeq = Seq.map (fun (x, _) -> x) s
     let xMean = Seq.average xSeq
     let xDif = Seq.map (fun i -> i - xMean) xSeq
@@ -17,12 +17,12 @@ let slr (s : (float * float) seq) : float -> float=
     let linearFun (m : float) (n : float) (x : float) : float = x * m + n
     linearFun m n
 
-let linearRegressionFunction = slr dataSet
+let linearRegressionFunction = simpleLinearRegression dataSet
 
 let chart = Chart.Combine(
                [ Chart.Line ([for x in 1. .. 5. -> x, linearRegressionFunction x])
-                 Chart.Point ([for i in 0 .. dataSet.Length - 1 -> Seq.item i dataSet])]
+                 Chart.Point ([for i in 0 .. dataSet.Length - 1 -> Seq.item i dataSet]).]
                ).WithXAxis(Title = "X-Axis", Min = 0.0, Max = 6.0)
-               .WithYAxis (Title = "Y-Axis", Min = 0.5)
+               .WithYAxis (Title = "Y-Axis", Min = 0.5).w
 
 System.Windows.Forms.Application.Run(chart.ShowChart())
