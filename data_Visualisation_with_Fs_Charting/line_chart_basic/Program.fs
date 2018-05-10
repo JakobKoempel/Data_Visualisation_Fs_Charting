11cf5fb3232205b8d09013a6b2100f5cb082a47c
Jakob Kömpel <jakob.koempel@gmail.com>
2018-05-10 14:35:29 +0200

    removed unnecessary line chart names

--- data_Visualisation_with_Fs_Charting/line_chart_basic/Program.fs
+++ data_Visualisation_with_Fs_Charting/line_chart_basic/Program.fs
@@ -6,9 +6,9 @@ let rec factorial (n : int) : int =
 
 let combine = Chart.Combine(
                   [
-                    Chart.Line([for x in 1..10 -> x , x * x], Name = "quadradic function")
-                    Chart.Line ([for x in 1..5 -> x, factorial x], Name = "factorial function")
-                    Chart.Line ([for x in 1..10 -> x , x], Name = "linear function")
+                    Chart.Line [for x in 1..10 -> x * x]
+                    Chart.Line [for x in 1..5 -> factorial x]
+                    Chart.Line [for x in 1..10 -> x]
                   ])
 
 System.Windows.Forms.Application.Run(combine.ShowChart())
