ce1ed3c243cb33465b44d18450781359ae29a072
Jakob Kömpel <jakob.koempel@gmail.com>
2018-05-28 08:19:16 +0200

    gun ownership murder rate correlation

---
+++ data_analysis/gun_ownership_murder_rate_correlation/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace gun_ownership_murder_rate_correlation.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("gun_ownership_murder_rate_correlation")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("gun_ownership_murder_rate_correlation")>]
+[<assembly: AssemblyCopyright("Copyright ©  2018")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("b9325753-b97a-4292-92f9-bafdd27e0a3d")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ data_analysis/gun_ownership_murder_rate_correlation/Program.fs
@@ -0,0 +1,34 @@
+﻿open System.IO
+open FSharp.Charting
+
+let countryData = File.ReadAllLines "gun_ownership_and_homocide_rate.txt"
+                  |> Seq.map (fun s -> s.Split [|' '|] )
+                  |> Seq.map (fun array -> (float array.[Seq.length array - 2], float array.[Seq.length array - 3]))
+
+let SLR (s : (float * float) seq) : float -> float =
+    let xSeq = Seq.map (fun (x, _) -> x) s
+    let xMean = Seq.average xSeq
+    let xDif = Seq.map (fun i -> i - xMean) xSeq
+    let xDifSquaredSum = Seq.sumBy (fun i -> i ** 2.0) xDif
+    let ySeq = Seq.map (fun (_, y) -> y) s
+    let yMean = Seq.average ySeq
+    let xDifTimesyDifSum = Seq.map2 (fun x y -> x * (y - yMean)) xDif ySeq |> Seq.sum
+
+    let m = xDifTimesyDifSum / xDifSquaredSum
+    let n = yMean - xMean * m
+    
+    let linearFun (m : float) (n : float) (x : float) : float = x * m + n
+    linearFun m n
+
+let linearRegressionFunction = SLR countryData
+
+let chart = Chart.Combine(
+                 [Chart.Point ([for i in 0 .. (countryData |> Seq.length) - 1 -> Seq.item i countryData])
+                  Chart.Line ([for x in 0 .. 100 -> x, linearRegressionFunction (float x)])]
+                 ).WithTitle("gun ownership - homocide correlation")
+                 .WithXAxis(MajorGrid=ChartTypes.Grid(Enabled=false), Title = "privatly owned guns per capita", Min = 0., Max = 110.)
+                 .WithYAxis (MajorGrid=ChartTypes.Grid(Enabled=false), Title = "homocide rate", Min = 0., Max = 120.)                
+
+System.Windows.Forms.Application.Run(chart.ShowChart())
+
+System.Console.ReadKey() |> ignore
